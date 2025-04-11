9999f4252e3b18aaf17c0963fe2cd6a524e86d86
richardborgesdev <richardborgesdev@gmail.com>
2025-04-11 09:31:44 -0300

    chore: shuffling answers, finish quiz

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,20 +1,35 @@
 import { useState } from "react";
 import QUESTIONS from "./questions";
+import quizCompletedImage from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
+  const isQuizFinished = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswers) => [...prevUserAnswers, selectedAnswer]);
   }
+
+  if (isQuizFinished) {
+    return (
+      <div id="summary">
+        <img src={quizCompletedImage} alt="throphy icon" />
+        <h2>Quiz completed</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers].sort(
+    () => Math.random() - 0.5
+  );
 
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
